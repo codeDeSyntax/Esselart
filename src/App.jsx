652ef1329b307693db648b6bf785f6582d00a2b1
Webkat <webket49@gmail.com>
2024-01-25 02:38:19 +0000

    minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -38,9 +38,9 @@ function App() {
 
         </nav>
 
-        <section className='flex flex-col p-6  sm:flex-row sm:p-8  sm:gap-4 h-[80vh] '>
+        <section className='flex flex-col p-6  sm:flex-row sm:p-8  sm:gap-4 h-[80vh]  m-auto'>
          
-        <div className='pageText flex flex-col items-center justify-center  sm:w-[45%] gap-8 mb-10 '>
+        <div className='pageText flex flex-col items-center justify-center  sm:w-[45%] gap-8 mb-10 m-auto'>
         
           <h1 className=' text-2xl sm:text-3xl font-bold text-center text-[poppins] text-violet-600 lg:text-[35px] '>Making <span className='text-white '>abstract , imaginary</span> and objects into real pencil arts !</h1>
           <button className='sm:py-2 sm:px-3 py-2 px-4 text-2xl text-violet-500 border w-[50%]  rounded-full hover:bg-white hover:cursor-pointer transition text-center '  >
@@ -53,7 +53,7 @@ function App() {
         </div>
         
         </div>
-        <div className='bg-cover  bg-[url("./Images/art.jpg")]  sm:w-[60%]  h-[90vh]  w-[90%] sm:flex sm:justify-center md:h-[50vh] lg:h-[74vh] lg:w-[55%] flex justify-center m-auto rounded-md'>
+        <div className='bg-cover  bg-[url("./Images/art.jpg")]  sm:w-[60%]  h-[90vh]  w-[90%]  md:h-[50vh] lg:h-[74vh] lg:w-[55%] flex  m-auto rounded-md'>
         <FaStar  color='white' className='animate-spin transition text-xl blur-sm'/>
         <FaStar size={30} color='white' className='animate-spin transition text-xl blur-sm' />
         </div>
